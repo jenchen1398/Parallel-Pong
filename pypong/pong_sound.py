4a7b578b33343775f3a779c70eabc6c2850c75cd
jenchen1398 <jen.chen.1398@gmail.com>
2015-08-05 10:20:45 -0700

    Update pong_sound.py

--- pypong/pong_sound.py
+++ pypong/pong_sound.py
@@ -1,7 +1,8 @@
 import pygame
 
-pygame.mixer.preinit(frequency=22050, size=-16, channels=2, buffersize=2048)
+pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffer=2048)
 pygame.mixer.init()
+
 def paddle_hit(window):
     collision_song = 'home/pi/collision_song.mp3'
     pygame.mixer.music.load(collision_song)
